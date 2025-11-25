1819ce3976b386c1c3f15ef02bc68bf6c15ebbb6
sebastianhernandezminano <sebastianhernandezminano@gmail.com>
2025-11-25 05:26:56 -0500

    logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Editor from '@monaco-editor/react';
 import axios from 'axios';
 import './App.css';
-import logo from "./assets/logo.png"
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 const DEFAULT_CODE = `fn main() {
@@ -13,6 +12,7 @@ const DEFAULT_CODE = `fn main() {
 }`;
 
 function App() {
+  const logo = "https://res.cloudinary.com/devuyzizr/image/upload/v1764066333/ChatGPT_Image_25_nov_2025_04_17_03_vucqd3.png"
   const [code, setCode] = useState(DEFAULT_CODE);
   const [compilationId, setCompilationId] = useState(null);
   const [loading, setLoading] = useState(false);
